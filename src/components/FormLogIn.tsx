7905490ac2d8ab02038b4e271f45f9c61c3362fe
Nastya Parnet <anastasia.parnet@incode-it.com>
2022-04-07 14:41:40 +0300

    EXP-2 CHANGE: FormLogIn

--- src/components/FormLogIn.tsx
+++ src/components/FormLogIn.tsx
@@ -1,7 +1,70 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, TextField } from '@mui/material';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
-const FormLogIn = () => {
-    return <div>FormLogIn</div>;
+interface InterfaceLogIn {
+    username: string;
+    password: string;
+}
+
+const validationSchema = yup.object({
+    username: yup.string().required("Введіть ім'я користувача"),
+    password: yup.string().min(4).max(16).required('Введіть пароль'),
+});
+
+const FormLogIn: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        getValues,
+    } = useForm<InterfaceLogIn>({
+        mode: 'onChange',
+        resolver: yupResolver(validationSchema),
+    });
+
+    const submitForm = (data: InterfaceLogIn) => {
+        console.log(data);
+    };
+
+    return (
+        <form className="login_form">
+            <div className="login_form_input">
+                <TextField
+                    label="Ім'я користувача"
+                    variant="filled"
+                    {...register('username')}
+                    error={errors?.username ? true : false}
+                    helperText={
+                        errors?.username
+                            ? errors.username.message || 'Error!!!'
+                            : ''
+                    }
+                />
+            </div>
+            <div className="login_form_input">
+                <TextField
+                    label="Пароль"
+                    type="password"
+                    variant="filled"
+                    {...register('password')}
+                    error={errors?.password ? true : false}
+                    helperText={
+                        errors?.password
+                            ? errors.password.message || 'Error!!!'
+                            : ''
+                    }
+                />
+            </div>
+            <div className="login_form_input">
+                <Button variant="text" onClick={handleSubmit(submitForm)}>
+                    Ввійти
+                </Button>
+            </div>
+        </form>
+    );
 };
 
 export default FormLogIn;
